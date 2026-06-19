e5d5bcf666e184852f38bc6941776e98ed7b8499
hope936 <3285721722@qq.com>
2026-06-19 18:56:20 +0800

    Delete 个人网站 directory

--- 个人网站/js/msg.js
+++
@@ -1,9 +0,0 @@
-function getMsgList(){
-    return JSON.parse(localStorage.getItem("msgList")) || [];
-}
-function addMsg(name,content){
-    let list = getMsgList();
-    let time = new Date().toLocaleString();
-    list.push({name,content,time});
-    localStorage.setItem("msgList",JSON.stringify(list));
-}
